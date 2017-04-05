f692b6da2722ad818e9a2594b54992460725cdf2
Tom Norton <thomas.norton@cyberbotics.com>
2017-04-05 13:31:11 +0200

    remove dependencies on node modules & stuff

--- js/renderer.js
+++ js/renderer.js
@@ -5,26 +5,24 @@ const isOnline = require('is-online');
 const tempUrl = "http://api.openweathermap.org/data/2.5/weather"
 const weatherApiKey = "d878ff95dda20eadb8502ada8fd89ecd";
 
-isOnline(1000).then(online => {
-    var locDOM = document.getElementById("location");
-    var tempDOM = document.getElementById("temp");
-    var coords = {lat: "", lon: ""};
-    if (online) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            coords.lat = position.coords.latitude;
-            coords.lon = position.coords.longitude;
-            let weatherReqUrl = tempUrl + "?" + "lat=" + coords.lat + "&lon=" + coords.lon + "&" + "APPID=" + weatherApiKey + "&units=metric";
-            fetch(weatherReqUrl)
-            .then((resp) => resp.json()) // Transform the data into json
-            .then(function(data) {
-                weather = data;
-                tempDOM.innerHTML = parseInt(weather.main.temp) + "&#176;";
-                locDOM.innerHTML = weather.name + ", " + weather.sys.country;
-            })
-        });
-    }
-    else {
-        locDOM.innerHTML = "No Internet Connection :("
-    }
+var locDOM = document.getElementById("location");
+var tempDOM = document.getElementById("temp");
+var coords = {lat: "", lon: ""};
+navigator.geolocation.getCurrentPosition(
+    function(position) {
+    coords.lat = position.coords.latitude;
+    coords.lon = position.coords.longitude;
+    let weatherReqUrl = tempUrl + "?" + "lat=" + coords.lat + "&lon=" + coords.lon + "&" + "APPID=" + weatherApiKey + "&units=metric";
+    fetch(weatherReqUrl)
+    .then((resp) => resp.json()) // Transform the data into json
+    .then(function(data) {
+        weather = data;
+        console.log(weather);
+        tempDOM.innerHTML = parseInt(weather.main.temp) + "&#176;";
+        locDOM.innerHTML = weather.name + ", " + weather.sys.country;
+    })
+},  function(){
+    locDOM.innerHTML = "No Internet Connection :("
 });
+
 
