5c2064cd6c9f3c199c2745e6dd43710577b2426b
Tom Norton <thomas.norton@cyberbotics.com>
2017-04-05 09:11:42 +0200

    trim down renderer

--- js/renderer.js
+++ js/renderer.js
@@ -2,25 +2,8 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 const isOnline = require('is-online');
-var loc,weather;
-const locUrl = "https://www.googleapis.com/geolocation/v1/geolocate?key=";
-const cityUrl = "https://maps.googleapis.com/maps/api/geocode/json"
 const tempUrl = "http://api.openweathermap.org/data/2.5/weather"
 const weatherApiKey = "d878ff95dda20eadb8502ada8fd89ecd";
-const geolocationApiKey = "AIzaSyCqSSjwgqpY4EMwtLgvlFF4tf0KH5yMx2M"
-// const geocodingApiKey = "AIzaSyAkoWK8DQYxvG98WIEqKs06yl4b2Cqf-SU"
-
-function getCityFromGeocodeResponse(resp) {
-  return "hello"
-}
-
-function getLocalityFromGeocodeResponse() {
-  return "bruh"
-}
-
-function getCountryFromGeocodeResponse() {
-  return "hej"
-}
 
 isOnline(1000).then(online => {
     var locDOM = document.getElementById("location");
